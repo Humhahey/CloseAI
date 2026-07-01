5f2ced8320bb11843bb4b12c7557583e05a57cae
lizili <lizili.gy@ccbft.com>
2026-07-01 17:39:43 +0800

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+from value import Value
+
+x = Value(1)
+a = Value(2)
+b = Value(3)
+
+y = a * x + b
+
+print(y)
---
+++ value.py
@@ -0,0 +1,12 @@
+class Value:
+    def __init__(self, data):
+        self.data = data
+
+    def __add__(self, other):
+        return Value(self.data + other.data)
+
+    def __mul__(self, other):
+        return Value(self.data * other.data)
+
+    def __repr__(self):
+        return str(self.data)
